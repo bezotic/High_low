e21cdb2204f05436ad19388a232e1db89eb1812d
Richard <Richard@Richards-MacBook-Pro-2.local>
2016-02-29 13:00:03 -0600

    finished project. fixed bug

--- highlow.php
+++ highlow.php
@@ -1,1 +1,23 @@
 <?php
+
+$random = mt_rand(1, 100);
+fwrite(STDOUT, " Guess a number\n ");
+
+
+do {
+
+	$number = fgets(STDIN);
+	$number_pressed = trim($number);
+	if ($number_pressed < $random) {
+		fwrite(STDOUT, "Higher\n");
+	} else if ($number_pressed > $random) {
+		fwrite(STDOUT, "lower\n");
+	} 
+	else if ($number_pressed == $random) {
+		fwrite(STDOUT, "YOU did IT!");
+	}
+
+}while($number != $random);
+
+
+
